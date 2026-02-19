998f86ef7e01164368968f144160335661c8e585
shivateja764 <dharmajishivatejashivateja4@gmail.com>
2026-02-19 13:24:59 +0530

    backend initial code setup

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express=require("express")
+const app =express()
+require("dotenv").config()
+const {connectDB}=require("./config/db.js")
+
+connectDB()
+
+app.get("/",(req,res)=>{
+   res.send("hi, iam running fine") 
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log("server started on port " + process.env.PORT)
+})
