04eb482b3a3ca3162cab231d91bcb3a07eeeadbe
shivateja764 <dharmajishivatejashivateja4@gmail.com>
2026-03-22 21:56:45 +0530

    jwt corrected

--- client/ConnectSphere/src/api/axiosInstance.js
+++ client/ConnectSphere/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8181/api/v1",
-  // baseURL: "https://connectsphere-apis.onrender.com/api/v1",
+  // baseURL: "http://localhost:8181/api/v1",
+  baseURL: "https://connectsphere-apis-7gl5.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
